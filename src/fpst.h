f51c3be492097e8edaf97fbd84614e25dde4b5aa
Frank Denis <github@pureftpd.org>
2018-02-11 01:53:32 +0100

    Avoid a redefinition of the FPST type

--- src/fpst.h
+++ src/fpst.h
@@ -5,7 +5,9 @@
 #include <stdlib.h>
 
 /** A trie */
+#ifndef fpst_GLOBALS
 typedef struct FPST FPST;
+#endif
 
 /** Type of the function pointer for `fpst_free()` */
 typedef void (*FPST_FreeFn)(const char *key, uint32_t val);
